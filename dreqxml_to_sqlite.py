5d8a7bc3d279a3809a215c9fc712309e00e0783f
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-03-31 14:18:56 +0200

    Improvements

--- dreqxml_to_sqlite.py
+++ dreqxml_to_sqlite.py
@@ -38,8 +38,8 @@ def field_statement(row_attribute, lab_unique):
         if foreign_table in [None, '']:
             raise RuntimeError(
                 "Found foreign key with unspecified table {}".format(name))
-        constraint = ('  REFERENCES uids (uid) -- Real table: {}'
-                      ''.format(foreign_table))
+        constraint = (f'  REFERENCES {foreign_table} (uid)'
+                      f'  -- Real table: {foreign_table}')
         field_stmt = "\n".join([field_stmt, constraint])
     return (name, field_stmt)
 
@@ -62,9 +62,9 @@ def format_table_definition(table):
         for row_attribute in row_attributes])
     field_names = principle_field_names + other_field_names
     field_statements = principle_field_stmts + other_field_statements
-    create_stmt = ("CREATE TABLE {} (\n  {}\n  );\n"
-                   "".format(name,
-                             "\n  ,".join(field_statements)))
+    create_stmt = (f"CREATE TABLE {name} (\n  {{}}\n  );\n"
+                   # f"INSERT INTO {name} (uid) VALUES (null);\n"
+                   "\n".format("\n  ,".join(field_statements)))
     return (name, level, (field_names, create_stmt))
 
 
@@ -75,14 +75,20 @@ def field_insert_statement(item, field_names):
         value = item.attrib.get(name, None)
         if value is not None:
             columns.append(name)
-            values.append(value.replace('"', ''))
+            value = value.replace('"', '')
+            if value == '':
+                value = 'null'
+            else:
+                value = f'"{value}"'
+            values.append(value)
     columns_stmt = '"'+'", "'.join(columns)+'"'
-    values_stmt = '"'+'", "'.join(values)+'"'
+    values_stmt = ', '.join(values)
     return values, columns_stmt, values_stmt
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output-file', default='dreq.sql')
     parser.add_argument('-d', '--dreq-dir', required=True)
     return parser.parse_args()
 
@@ -104,48 +110,15 @@ def prepare_table_definitions(dreq_dir):
     return table_defs
 
 
-def emit_header():
-    print("PRAGMA foreign_keys = ON;")
-    print("CREATE TABLE uids (uid TEXT PRIMARY KEY, table_name TEXT);")
+def emit_header(out):
+    out.write("PRAGMA foreign_keys = ON;\n")
+    out.write("CREATE TABLE uids (uid TEXT PRIMARY KEY, table_name TEXT);\n")
+    out.write("INSERT INTO uids VALUES (null, null);\n")
 
 
-def emit_insertions(dreq_dir, filename, table_defs, ordered_names):
-    data_doc = ET.parse(os.path.join(dreq_dir, filename)).getroot()
-    main = data_doc.find(NS_PREF_DREQ.format('main'))
-    for name in ordered_names:
-        print(table_defs[name][1])
-    print('BEGIN TRANSACTION;')
-    for name in ordered_names:
-        section = main.find(NS_PREF_DREQ.format(name))
-        field_names = table_defs[name][0]
-        for item in section.findall(NS_PREF_DREQ.format('item')):
-            v, cols, vals = field_insert_statement(item, field_names)
-            print('INSERT INTO uids VALUES ("{}", "{}");'.format(
-                v[0], name))
-    print('COMMIT TRANSACTION;')
-    print('BEGIN TRANSACTION;')
-    for name in ordered_names:
-        section = main.find(NS_PREF_DREQ.format(name))
-        field_names = table_defs[name][0]
-        for item in section.findall(NS_PREF_DREQ.format('item')):
-            v, cols, vals = field_insert_statement(item, field_names)
-            print('INSERT INTO {} ({}) VALUES ({});'.format(
-                name, cols, vals))
-    print('COMMIT TRANSACTION;')
-
-
-def main():
-    args = parse_args()
-    dreq_dir = args.dreq_dir
-
-    table_defs = prepare_table_definitions(dreq_dir)
-
-    emit_header()
-
-    ordered_names = ['units']
-    emit_insertions(dreq_dir, 'dreqSupp.xml', table_defs, ordered_names)
-
+def emit_table_defs(out, table_defs):
     ordered_names = (
+        'units',
         'remarks',
         'exptgroup',
         'mip',
@@ -172,7 +145,75 @@ def main():
         'timeSlice',
         'requestItem',
     )
-    emit_insertions(dreq_dir, 'dreq.xml', table_defs, ordered_names)
+    for name in ordered_names:
+        out.write(table_defs[name][1])
+
+
+def emit_insertions(out, dreq_dir, filename, table_defs, ordered_names):
+    data_doc = ET.parse(os.path.join(dreq_dir, filename)).getroot()
+    main = data_doc.find(NS_PREF_DREQ.format('main'))
+    out.write('BEGIN TRANSACTION;\n')
+    for name in ordered_names:
+        section = main.find(NS_PREF_DREQ.format(name))
+        field_names = table_defs[name][0]
+        for item in section.findall(NS_PREF_DREQ.format('item')):
+            v, cols, vals = field_insert_statement(item, field_names)
+            out.write('INSERT INTO uids VALUES ({}, "{}");\n'.format(
+                v[0], name))
+    out.write('COMMIT TRANSACTION;\n')
+    out.write('BEGIN TRANSACTION;\n')
+    for name in ordered_names:
+        section = main.find(NS_PREF_DREQ.format(name))
+        field_names = table_defs[name][0]
+        for item in section.findall(NS_PREF_DREQ.format('item')):
+            v, cols, vals = field_insert_statement(item, field_names)
+            out.write('INSERT INTO {} ({}) VALUES ({});\n'.format(
+                name, cols, vals))
+    out.write('COMMIT TRANSACTION;\n')
+
+
+def main():
+    args = parse_args()
+    dreq_dir = args.dreq_dir
+
+    table_defs = prepare_table_definitions(dreq_dir)
+
+    with open(args.output_file, 'w') as out:
+        emit_header(out)
+        emit_table_defs(out, table_defs)
+
+        ordered_names = ('units',)
+        emit_insertions(out,
+                        dreq_dir, 'dreqSupp.xml', table_defs, ordered_names)
+
+        ordered_names = (
+            'remarks',
+            'exptgroup',
+            'mip',
+            'experiment',
+            'miptable',
+            'modelConfig',
+            'objective',
+            'requestVarGroup',
+            'requestLink',
+            'objectiveLink',
+            'standardname',
+            'var',
+            'varChoice',
+            'varChoiceLinkC',
+            'varChoiceLinkR',
+            'spatialShape',
+            'temporalShape',
+            'cellMethods',
+            'structure',
+            'CMORvar',
+            'tableSection',
+            'requestVar',
+            'grids',
+            'timeSlice',
+            'requestItem',
+        )
+        emit_insertions(out, dreq_dir, 'dreq.xml', table_defs, ordered_names)
 
 
 if __name__ == '__main__':
